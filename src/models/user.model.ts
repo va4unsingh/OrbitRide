56043358d4fe81cd1ac0830ccebd2307ac5069b5
Varun Singh <varunsingh7004@gmail.com>
2025-08-22 19:09:52 +0530

    User Model done

---
+++ src/models/user.model.ts
@@ -0,0 +1,53 @@
+import mongoose, { Document } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser extends Document {
+  fullname: {
+    firstname: string;
+    lastname: string;
+  };
+  email: string;
+  password: string;
+  socketId?: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>({
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
+    },
+    lastname: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
+    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [5, "Email must be at least 5 characters long"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  socketId: {
+    type: String,
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<IUser>) ||
+  mongoose.model<IUser>("User", userSchema);
+
+export default UserModel;
