fa8b2a6db71ee5491ef58fe847240067fa071572
Varun Singh <varunsingh7004@gmail.com>
2025-08-22 23:20:14 +0530

    boiler plate added for auth js

---
+++ src/auth.ts
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize({ request }) {
+        const response = await fetch(request);
+        if (!response.ok) return null;
+        return (await response.json()) ?? null;
+      },
+    }),
+  ],
+});
