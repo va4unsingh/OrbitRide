14f08ec63227ed08dcf5f7b166094852361cb17c
Varun Singh <varunsingh7004@gmail.com>
2025-08-23 00:24:51 +0530

    wrote callbacks and pages

--- src/auth.ts
+++ src/auth.ts
@@ -1,20 +1,72 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import dbConnect from "./lib/dbConnect";
+import UserModel, { IUser } from "./models/user.model";
+import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
+      id: "credentials",
+      name: "Credentials",
       credentials: {
-        username: { label: "Username" },
+        email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      async authorize({ request }) {
-        const response = await fetch(request);
-        if (!response.ok) return null;
-        return (await response.json()) ?? null;
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = (await UserModel.findOne({
+            email: credentials.email,
+          }).select("+password")) as IUser | null;
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+
+          if (typeof user.password !== "string") {
+            throw new Error("Invalid user data");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect password");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
       },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.email = user.email;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id as string;
+        session.user.email = token.email as string;
+      }
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.AUTH_SECRET,
 });
